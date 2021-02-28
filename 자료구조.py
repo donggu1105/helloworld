4b84fad76484dc6f97d47f1ef683b0eb3d7a1103
donggu1105 <kbh11051@gmail.com>
2021-02-28 11:27:47 +0900

    자료구조 - 튜플

--- 자료구조.py
+++ 자료구조.py
@@ -107,3 +107,21 @@ print(cabinet.items())
 cabinet.clear()
 print(cabinet)
 
+############### 튜플 ###############
+print("#################### 튜플 ################")
+
+#변경되지않는 목록을 이용할때 튜플 사용
+
+menu = ("돈까스", "치즈까스")
+print(menu)
+print(menu[0])
+print(menu[1])
+
+
+name = "김종국"
+age = 20
+hobby = "코딩"
+print(name,age,hobby)
+
+# 튜플 사용하는 예제
+(name , age , hobby) = ("김종국", 20 ,"코딩")
