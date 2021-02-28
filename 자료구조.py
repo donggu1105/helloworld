634ace8eb4a0e6fb50628c6a639bb9302865247a
donggu1105 <kbh11051@gmail.com>
2021-02-28 11:33:17 +0900

    자료구조 - 세트(집합)

--- 자료구조.py
+++ 자료구조.py
@@ -125,3 +125,34 @@ print(name,age,hobby)
 
 # 튜플 사용하는 예제
 (name , age , hobby) = ("김종국", 20 ,"코딩")
+
+############### 세트 ###############
+print("#################### 세트 ################")
+
+# 집합 (set)
+# 중복 안됨 , 순서 없음
+
+my_set = {1,2,3,4,5}
+print(my_set)
+
+java = {"유재석", "김태호", "양세형"}
+python = set(["유재석", "박명수"])
+
+# 교집합 (자바 파이썬 둘다 가능한 사람)
+print(java & python)
+print(java.intersection(python))
+
+# 합집합 (java 도 할수있거나 python도 할수있는 개발자)
+print(java | python)
+print(java.union(python))
+
+# 차집합 (java는 할줄알지만 python은 할줄모르는 개발자)
+print(java - python)
+print(java.difference(python))
+
+# python을 할 줄 아는 사람이 늘어남
+python.add("김태호")
+print(python)
+
+#java를 잊었어요
+java.remove("김태호")
