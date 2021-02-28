bba237ce57788d22c33daa3923a3145897eb7e53
donggu1105 <kbh11051@gmail.com>
2021-02-28 10:23:22 +0900

    문자열 처리 - 퀴즈

--- 문자열처리.py
+++ 문자열처리.py
@@ -86,3 +86,17 @@ print("Redd\bApple")
 
 # \t : 탭
 print("Red\tApple")
+
+# Quiz
+
+url = "http://naver.com"
+
+my_str = url.replace("http://", "")
+print(my_str)
+my_str = my_str[:my_str.index(".")]
+print(my_str)
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print(password)
+
+
+print("{0} 의 비밀번호는 {1} 입니다.".format(url,password))
