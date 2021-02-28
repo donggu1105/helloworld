38d3050065881153f41525c907aca5b5987e9a9c
donggu1105 <kbh11051@gmail.com>
2021-02-28 09:39:15 +0900

    문자열 처리 - 문자열 처리함수

--- 문자열처리.py
+++ 문자열처리.py
@@ -13,3 +13,21 @@ print("뒤부터 세기 : "+ jumin[-7:]) # 뒤로부터세기 -7부터 끝까지
 
 #문자열 처리함수
 print("########## 문자열 처리함수 ############")
+
+python = "Python is Amazing"
+
+print(python.lower())
+print(python.upper())
+print(python[0].isupper()) # 몇번째 자리 대문자
+print(len(python)) # 길이반환
+print(python.replace("Python", "Java")) # 문자 찾은뒤 새로운 문자열로 대체
+
+
+index = python.index("n")
+print(index) # "?" 안에 든거 index알려줌
+index = python.index("n", index + 1) # index+1이 시작위치 => 그 뒤로나오는 n을 찾는다
+print(index)
+
+print(python.find("n")) # 찾아서 인덱스 반환
+print(python.find("Java")) # 없으면 -1 반환
+# print(python.index("Java")) # 하면 오류
