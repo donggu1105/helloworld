1ff339cc022fd4106e2ee8d6c5d0691ae8fa2fc5
donggu1105 <kbh11051@gmail.com>
2021-03-09 17:40:54 +0900

    python basic

---
+++ practice.py
@@ -0,0 +1,13 @@
+# 1
+print("Hello World")
+# 2
+print("Mary\'s cosmetics")
+# 3 x (소수점 둘째짜리까지 표현하기)
+digit = 3.141592
+print("%.2f" % digit)
+print("%.4f" % digit)
+# 4
+# first = input("첫번째 숫자를 입력하세요")
+# second = input("두번째 숫자를 입력하세요")
+# print(int(first) + int(second))
+# 5
